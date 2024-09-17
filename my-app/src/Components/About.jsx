4cec1ab409ef8c9e917ca23243c97044daba3f06
Nafisat Akokhia <156707230+naphydeby@users.noreply.github.com>
2024-09-17 14:39:19 -0700

    Home page edited

--- my-app/src/Components/About.jsx
+++ my-app/src/Components/About.jsx
@@ -2,17 +2,17 @@ function About() {
   return (
     <>
       <div>
-        <img src="./images/school.jpeg" className={`w-full h-auto pt-12`} />
-        <div className={`mt-[-60px] lg:ml-20 w-full `}>
-          <p className={`font-bold lg:text-2xl text-gray-400`}>About Us</p>
+        <img src="./images/school.jpeg" className="w-full h-auto pt-12" />
+        <div className='mt-[-60px] lg:ml-20 w-full'>
+          <p className='font-bold lg:text-2xl text-gray-400'>About Us</p>
         </div>
       </div>
-      <div className={`lg:flex`}>
-        <div className={`mt-10 ml-10 text-gray-600 `}>
-          <h2 className={` font-sm text-2xl lg:text-4xl`}>Introduction</h2>
-          <hr className={`mt-2 w-[80px] font-bold border-gray-600 border-4`} />
-          <div className={`w-[60vw]`}>
-            <p className={`mt-8 `}>
+      <div className='  flex flex-col lg:flex-row justify-evenly items-center '>
+        <div className=' text-gray-600 '>
+          <h2 className='font-sm text-2xl lg:text-4xl mt-10 lg:mt-0'>Introduction</h2>
+          <hr className='mt-2 w-[80px] font-bold border-gray-600 border-4' />
+          <div className=" w-[80vw] lg:w-[60vw]" >
+            <p className='mt-8 '>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Asperiores vitae vel consequatur adipisci recusandae fugiat labore
               fuga omnis mollitia saepe temporibus dolor exercitationem, ex,
@@ -24,15 +24,15 @@ function About() {
             </p>
           </div>
         </div>
-        <div className={`mt-10 lg:mr-10 ml-10 lg:ml-0 `}>
-          <h2 className={`text-gray-600 font-sm text-2xl lg:text-4xl`}>Mission</h2>
-          <hr className={`mt-2 w-[80px] font-bold border-gray-600 border-4`} />
+        <div className='mt-4 lg:mt-[150px] lg:mr-4 '>
+          <h2 className='text-gray-600 font-sm text-2xl lg:text-4xl'>Mission</h2>
+          <hr className='mt-2 w-[80px] font-bold border-gray-600 border-4' />
           <div
-            className={`mt-10 w-[50vw] lg:w-[30vw] h-auto  border-gray-600 border-4 text-gray-600 `}
+            className='mt-10 w-[80vw] lg:w-[30vw] h-auto  border-gray-600 border-4 text-gray-600'
           >
-            <p className={`mt-2 ml-4 font-bold `}>Our Vision</p>
-            <hr className={`mt-4 w-full font-bold border-gray-600 border-2`} />
-            <p className={`m-4`}>
+            <p className='mt-2 ml-4 font-bold'>Our Vision</p>
+            <hr className='mt-4 w-full font-bold border-gray-600 border-2' />
+            <p className='m-4'>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic qui,
               dicta nostrum beatae porro placeat quae iusto eligendi sapiente
               earum consequuntur magnam recusandae quidem itaque perspiciatis
@@ -40,21 +40,21 @@ function About() {
             </p>
           </div>
           <div
-            className={`mt-2  w-[50vw] lg:w-[30vw] h-auto  border-gray-600 border-4 text-gray-600 `}
+            className='mt-2  w-[80vw] lg:w-[30vw] h-auto  border-gray-600 border-4 text-gray-600'
           >
-            <p className={`mt-2 ml-4 font-bold `}>Our Mission Statement</p>
+            <p className='mt-2 ml-4 font-bold'>Our Mission Statement</p>
           </div>
         </div>
       </div>
-      <div className={`mt-10 ml-10 text-gray-600 `}>
-        <h2 className={` font-sm text-2xl lg:text-4xl`}>Membership/Partnership</h2>
-        <hr className={`mt-2 w-[80px] font-bold border-gray-600 border-4`} />
-        <div className={`flex mt-12`}>
-          <div className={`w-[30vw] border-8 border-gray-600`}>
+      <div className='mt-10 ml-10 text-gray-600'>
+        <h2 className='font-sm text-2xl lg:text-4xl'>Membership/Partnership</h2>
+        <hr className='mt-2 w-[80px] font-bold border-gray-600 border-4' />
+        <div className='flex mt-12'>
+          <div className='w-[30vw] border-8 border-gray-600'>
             <img src="./images/logo1.png" />
           </div>
-          <div className={`w-[30vw] border-8 border-gray-600 ml-8`}>
-            {" "}
+          <div className="w-[30vw] border-8 border-gray-600 ml-8">
+            
             <img src="./images/logo2.png" />
           </div>
         </div>
--- my-app/src/Components/Navbar.jsx
+++ my-app/src/Components/Navbar.jsx
@@ -2,13 +2,17 @@ import { Link} from "react-router-dom";
 function Navbar(){
     return(
         
-         <nav className="bg-gray-600 w-[100%]  lg:w-full h-20 text-white flex justify-between ">
-            <ul className="flex justify-between mt-6 ml-6  space-x-3 ">
+         <nav className="bg-gray-600 w-[100%]  lg:w-full h-20 text-white flex justify-between items-center pl-2 lg:pl-6 ">
+          
+            <ul className=" w-[45%] lg:w-[15%] flex justify-between ">
          <Link to= '/' className=" hover:text-blue-400">Home</Link>
          <Link to= 'about' className=" hover:text-blue-400 ">About</Link>
          <Link to= 'contact' className=" hover:text-blue-400">Contact</Link>
          </ul>
-         <Link to='signup' className="mr-6 mt-6 hover:text-blue-400">Signup</Link>
+         <div>
+         <Link to='signup' className=" mr-2 lg:mr-6 hover:text-blue-400">Signup</Link>
+         </div>
+        
             </nav>
     )
 }
